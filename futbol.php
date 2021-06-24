d5881608127a94be42a39735f4b739639d520f3e
Bodiker <Bodiker86@gmail.com>
2021-06-24 14:20:45 +0200

    The end

--- futbol.php
+++ futbol.php
@@ -16,15 +16,14 @@
         <h2>Światowe rozgrywki piłkarskie</h2>
         <img src="obraz1.jpg" alt="boisko">
     </header>
-    
-   <section id="mecze">
+    <section id="mecze">
        <?php
        $query = $db->prepare("SELECT zespol1, zespol2, wynik, data_rozgrywki FROM rozgrywka WHERE zespol1 = 'EVG'");
        
        $query->execute();
        $result = $query->get_result();
        while ($row = $result->fetch_assoc()) {
-           echo " <div id='mecz'>";
+           echo " <div id=\"mecz\">";
            $z1 =$row['zespol1']; 
            $z2 =$row['zespol2'];
            $w = $row['wynik'];
@@ -43,11 +42,26 @@
         <div id="lewy">
         <p>Podaj pozycje zawodników (1-bramkarze, 2-obrońcy, 3-pomocnicy, 4-napastnicy)</p>
         <form action="futbol.php" method="post">
-            <input type="number" name="" id="">
+            <input type="number" name="pozycja" id="">
             <button type="submit">Sprawdż</button>
         </form>
         <ul>
-          <li>skrypt2</li>  
+          <?php
+          if (isset($_REQUEST['pozycja']) && $_REQUEST['pozycja'] != "") {
+              $query = $db->prepare("SELECT imie, nazwisko FROM zawodnik WHERE pozycja_id = ?");
+              $query->bind_param("i", $_REQUEST['pozycja']);
+              $query->execute();
+              $result = $query->get_result();
+              while ($row = $result->fetch_assoc()) {
+                 $i = $row ['imie'];
+                 $n = $row ['nazwisko'];
+                 echo "<li>$i $n</li>";
+              }
+          }
+            
+
+          ?>
+
         </ul>
         </div>
         <div id="prawy">
